3f1db27b68b8df06bdaecb0383fdf37e79e9f035
jlashner <jlashner@gmail.com>
2018-09-27 13:22:01 -0700

    Changed curve format to be standard Lakeshore Format

--- ocs/Lakeshore/Curve.py
+++ ocs/Lakeshore/Curve.py
@@ -7,64 +7,55 @@ Created on Tue Feb 27 21:05:48 2018
 """
 
 import numpy as np
+from collections import OrderedDict
+
 class Curve:
     """
     Header for calibration curve
     ----------------
-    :Name:      Name of curve
-    :SN:        Serial Number
-    :Format:    2 = V:K, 3 = Ohms:K, 4 = log(Ohms):K
-    :Limit:     Temperature Limit (in K)
-    :Coeff:     1 = negative, 2 = positive
-    
+    :Sensor Model:      Name of curve
+    :Serial Number:        Serial Number
+    :Data Format:    2 = V:K, 3 = Ohms:K, 4 = log(Ohms):K
+    :SetPoint Limit:     Temperature Limit (in K)
+    :Temperature Coefficient:     1 = negative, 2 = positive
+    :Number of Breakpoints:     Number of curve points
     """
-    def __init__(self, filename=None, header=None, units=None, temps=None):
+    def __init__(self, filename=None, header=None, breakpoints=None):
 
         if filename is not None:
             self.load_from_file(filename)
         else:
-            self.header = header
-            self.units = units
-            self.temps = temps
+            if header and breakpoints:
+                self.header = header
+                self.breakpoints = breakpoints
+            else:
+                raise Exception("Must give either filename or header and breakpoints")
 
     def load_from_file(self, filename):
         with open(filename, 'r') as file:
-            # Reads Header
-            line_num = 0
-            while True:
-                line_num += 1
-                line = file.readline().strip()
-                if line == "[DATA]":
-                    break
-                
-                if ':' in line:
-                    key = line.split(':')[0].strip()
-                    val = line.split(':')[1].strip()
-                    self.header[key] = val
-                
-            _, self.units, self.temps = np.loadtxt(filename, skiprows=line_num, unpack=True)
+            content = file.readlines()
 
+        header = OrderedDict({})
+        for line in content:
+            if line.strip()=='':
+                break
+            key, v = line.split(':')
+            val = v.split('(')[0]
+            header[key] = val
 
-    def write_to_file(self, filename):
-        with open(filename, 'w') as file:
-            file.write("[HEADER]\n")
-            for key in ["Name", "SN", "Format", "Limit", "Coeff"]:
-                file.write("{}:\t{}\n".format(key, self.header[key]))
-            file.write("\n[DATA]\n")
-            file.write("# No.\tUnits\tTemp(K)\n\n")
-
-            for i in range(len(self.units)):
-                file.write("{}\t{}\t{}\n".format(i+1, self.units[i], self.temps[i]))
+        self.breakpoints = []
+        for line in content[9:]:
+            num, unit, temp = line.split()
+            self.breakpoints.append((float(unit), float(temp)))
 
     def __str__(self):
         string = ""
-        for k in self.header.keys():
-            string += "%-15s: %s\n"%(k, self.header[k])
+        for key, val in self.header.items():
+            string += "%-15s: %s\n"%(key, val)
         return string
-    
-    
+
 if __name__ == "__main__":
-    filename = "curves/defaults/SimSensorNTC.txt"
+    filename = "D-001.cal"
     curve = Curve(filename=filename)
     
     

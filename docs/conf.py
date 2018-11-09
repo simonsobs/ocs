b6d160780d4f09caa9a5fe93471cf3d316c60364
Matthew Hasselfield <mhasself@users.noreply.github.com>
2018-11-09 09:47:56 -0500

    Make docs compile without errors, out of the box.  In Makefile, define environment variable OCS_DOC_BUILD=True (borrowed from READTHEDOCS), and then suppress import of weird libraries (ahem spt3g) in ocs code so that docs can be built more easily.  Add sys.path.append(..) in to conf.py path, so agents and ocs can both be seen easily from there.

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.append(os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -77,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -88,7 +87,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.

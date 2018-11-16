137f9290066d312bbc201ae4f0eceb2f3137c0fd
Matthew Hasselfield <mhasself@users.noreply.github.com>
2018-11-16 09:45:42 -0500

    Complete the ocs/ submodule structure so it all installs properly.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import versioneer
 setup (name = 'ocs',
        description='Observatory Control System',
        package_dir={'ocs': 'ocs'},
-       packages=['ocs'],
+       packages=['ocs', 'ocs/Lakeshore', 'ocs/util'],
        version=versioneer.get_version(),
        cmdclass=versioneer.get_cmdclass())

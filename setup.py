bfe217b2c658153508830f090268cab936120b78
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-01-21 14:14:48 -0500

    ocsbow: minor improvements
    - Suppress a warning from wampy
    - Default to python3 instead of python2
    - Include in setup.py scripts= list

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup (name = 'ocs',
        description='Observatory Control System',
        package_dir={'ocs': 'ocs'},
        packages=['ocs', 'ocs/Lakeshore', 'ocs/util'],
+       scripts=['bin/ocsbow'],
        version=versioneer.get_version(),
        cmdclass=versioneer.get_cmdclass())

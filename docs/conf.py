498e05f63d9d96d802c53877427e0241184fbd87
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-03-15 09:51:41 -0400

    HostMaster: have ocsbow --help auto-render in docs.
    Also updates requirements.txt to include sphinx and sphinx-argparse.

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
 ]
+extensions += ['sphinxarg.ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

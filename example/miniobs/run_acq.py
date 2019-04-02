834bed55281fd1f619086737692959dbdfe49105
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-04-02 18:28:43 -0400

    example/miniobs: stand-alone mini-observatory.
    Assuming that pre-requisites are installed, this should run without
    setting up any additional infrastructure.  See README.

---
+++ example/miniobs/run_acq.py
@@ -0,0 +1,27 @@
+import ocs
+import time
+
+agg = ocs.site_config.get_control_client('agg1')
+faker = ocs.site_config.get_control_client('data1')
+
+if 1:
+    print('Aggregator -- initialize...')
+    agg.request('start', 'initialize')
+    print(agg.request('wait', 'initialize'))
+    print(' - start aggregator.')
+    agg.request('start', 'aggregate')
+
+if 1:
+    print('Data Faker -- start 30 second acq.')
+    faker.request('start', 'acq')
+    for i in range(30):
+        time.sleep(1)
+        print(faker.request('status', 'acq'))
+    faker.request('stop', 'acq')
+    print(faker.request('wait', 'acq'))
+
+if 1:
+    print(' stop aggregator.')
+    agg.request('stop', 'aggregate')
+
+

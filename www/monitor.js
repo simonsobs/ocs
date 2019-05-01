f0afab67d2f4e59d28d7ecddc01dda0f40e91734
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-05-01 15:43:39 -0400

    www/monitor.html: a not completely useless demo.

--- www/monitor.js
+++ www/monitor.js
@@ -1,134 +1,132 @@
-var connection = null;
-var session = null;
-var site_root = 'observatory';
+var debugs = {};         // For stashing debug data.
 
-var debugs = {};
+/* log(msg)
+ *
+ * This function must always me available, even if it does nothing.
+ */
 
-// A print function for debugging.
 function log(msg) {
-    var log = document.getElementById("messages");
-    log.innerHTML = msg + "<br>" + log.innerHTML;
+    var log = $("#messages");
+    log.html(log.html() + msg + '<br>');
+    log.scrollTop(log[0].scrollHeight - log.height());
 }
-
-function run_task(instance_id, task_name) {
-    log('Requesting start of task "' + task_name + '"....');
-    connection.session.call(site_root + "." + instance_id + '.ops', ['start', task_name]).then(
-        function (result) {
-            // err, msg, session = result.
-            log(result[1]);
-        });
-}
-
-function start_proc(instance_id, proc_name) {
-    log('Requesting start of process "' + proc_name + '"....');
-    connection.session.call(site_root + "." + instance_id + '.ops', ['start', proc_name]).then(
-        function (result) {
-            // err, msg, session = result.
-            log(result[1]);
-        });
-}
-
-function stop_proc(instance_id, proc_name) {
-    log('Requesting stop of process "' + proc_name + '"....');
-    connection.session.call(site_root + "." + instance_id + '.ops', ['stop', proc_name]).then(
-        function (result) {
-            // err, msg, session = result.
-            log(result[1]);
-        });
-}
-
-var status_class_map = {
-    'done': 'op_status_done',
-    'running': 'op_status_running',
-    'starting': 'op_status_starting',
-    'stopping': 'op_status_stopping'
-    };
-
-function update_status(source, session) {
-    var text = '<span class="op_name">' + session.op_name + '</span>';
-    var classstr = "op_status " + status_class_map[session.status];
-    text += '<span class="' + classstr + '">' + session.status + '</span>';
-    text += '<hr>';
-    text += '<div class="op_time_box"><p>';
-    t0 = get_date_time_strings(session.start_time);
-    t1 = get_date_time_strings(session.end_time);
-    text += '<table><tr><td>'
-          + 'Started:</td><td><span class="op_start_time">' + t0[0] + ' ' + t0[1] + '</span>'
-          + '</td></tr><tr><td>'
-          + 'Ended:</td><td><span class="op_end_time">' + t1[0] + ' ' + t1[1] + '</span>'
-          + '</td></tr></table>';
-    text += '</p></div>';
-    text += '<div>Messages:<br>';
-    var i = session.messages.length - 2;
-    while (i < session.messages.length) {
-        if (i>=0) {
-            var m = session.messages[i];
-            t0 = get_date_time_strings(m[0]);
-            text += '<span class="op_log_message">' + t0[1] + ':' + m[1] + '</span>';
-        }
-        i++;
-    }
-    text += '</div>';
-
-    var target = $('#status_' + session.op_name);
-    target.html(text);
-}
+ocs_log = log;
 
 function init() {
-    
     $(document).ready(function() {
-        log('starting.');
-
-        console.log("Ok, Autobahn loaded", autobahn.version);
-        var url = "ws://localhost:8001/ws";
-
-        connection = new autobahn.Connection({
-            url: url,
-            realm: "debug_realm"
-        });
-        connection.onopen = function(_session, details) {
-            session = _session;
-            log('connected.');
-            var pop_status = function (result) {
-                debugs['tlist'] = result;
-                result.forEach(function (row) {
-                    if (row[1] != null)
-                        update_status('feed', row[1]);
-                    log('Reloading ' + row[0]);
-                });
-            };
-            // session.call(agent_addr, ['get_tasks']).then(pop_status);
-            // session.call(agent_addr, ['get_processes']).then(pop_status);
-            
-            session.subscribe(agent_addr + '.feed', function (args, kwargs, details) {
-                debugs.feed = args;
-                update_status('feed', args[0]);
-            });
-            
-        };
-        connection.onclose = function(reason, details) {
-            log('closed.');
-        };
-
-        connection.open();
-        
-        log('done init.');
+        log('Page init function is running...');
+        log('Autobahn version:', autobahn.version);
+        // Set up defaults -- these could be fed from elsewhere.
+        $('#wamp_router').val('ws://localhost:8001/ws');
+        $('#wamp_realm').val('debug_realm');
+        $('#registry_addr').val('observatory.registry');
+        $('#target_agent').val('observatory.agg1');
+        connect();
     });
 }
 
-function get_date_time_strings(timestamp) {
-    function twodig(x) {
-        if (x<10) return '0'+x;
-        return x;
-    }
+function AgentList () {
+    this.agent_list = [];
+}
 
-    var d = new Date(parseInt(timestamp)*1000);
-    if (isNaN(d.getTime())) {
-        return ['??', '??'];
+AgentList.prototype = {
+    update_agent_info: function(agent_name) {
+        this.agent_list.push(agent_name);
+        text = '';
+        this.agent_list.forEach(function (x) {
+            text += x + '<br>';
+        });
+        $('#registry_dump').html(text);
     }
-    var datestr = d.getUTCFullYear() + '-' + twodig(d.getUTCMonth()+1) + '-'
-                + twodig(d.getUTCDate());
-    var timestr = twodig(d.getUTCHours()) + ':' + twodig(d.getUTCMinutes()) + ':' +
-                  twodig(d.getUTCSeconds());
-    return [datestr, timestr];
+}
+
+function connect() {
+    url = $('#wamp_router').val();
+    realm = $('#wamp_realm').val();
+
+    log('Connecting to "' + url + '", realm "' + realm + '"...');
+
+    ocs = new autobahn.Connection({
+	url: url,
+        realm: realm,
+    });
+    ocs.onopen = function(_session, details) {
+        ocs_log('connected.');
+        var agent_list = new AgentList();
+        reg_addr = $('#registry_addr').val();
+        debugs['reg'] = [];
+        ocs.session.subscribe(reg_addr + '.feeds.agent_activity',
+                              function (args, kwargs, details) {
+                                  args.forEach(function (x) {
+                                      addr = x[0][1]['agent_address'];
+                                      ocs_log(addr);
+                                      agent_list.update_agent_info(addr);
+                                  });
+                              });
+    };
+    ocs.onclose = function(reason, details) {
+        debugs.reason = reason;
+        ocs_log('closed.');
+    };
+    ocs.open();
+}
+
+function query_registry() {
+    reg_addr = $('#registry_addr').val();
+    reg_client = new AgentClient(ocs, reg_addr);
+    reg_client.run_task('dump_agent_info', []).then(
+        function (result) {log('dump_agent_info finished.'); });
+}
+
+function query_interface() {
+    if (ocs == null) return;
+    agent_addr = $('#target_agent').val();
+    client = new AgentClient(ocs, agent_addr);
+    client.scan(function () {
+        log('Client scan completed.');
+        iface_summary = 'Processes:<br>';
+        client.procs.forEach(function (x) {
+            iface_summary += '&nbsp;&nbsp;' + x[0] + '<br>';
+        });
+        iface_summary += '<br>Tasks:<br>';
+        client.tasks.forEach(function (x) {
+            iface_summary += '&nbsp;&nbsp;' + x[0] + '<br>';
+        });
+        iface_summary += '<br>Feeds:<br>';
+        client.feeds.forEach(function (x) {
+            iface_summary += '&nbsp;&nbsp;' + x[0] + '<br>';
+        });
+        $('#interface_summary').html(iface_summary);
+    });
+    debugs['client'] = client;
+
+    var msgh = new MessageBuffer();
+    msgh.callback = function () {
+        text = '';
+        for (i=Math.max(0, this.messages.length - 10); i < this.messages.length; i++) {
+            dt = get_date_time_strings(this.messages[i][0]);
+            text += dt[0] + ' ' + dt[1] + ' ' + this.messages[i][1] + '<br>\n';
+        }
+        $('#status_tasks').html(text);
+        log("Now there are " + this.messages.length + " messages.");
+    };
+
+    client.onSession = function (result) {
+        session = result[2];
+        if (session.op_name == 'dump_agent_info') {
+            msgh.update(session);
+        }
+    };
+}
+
+function query_operation() {
+    if (ocs == null) return;
+    agent_addr = $('#target_agent').val();
+    agent_op = $('#target_op').val();
+    client = new AgentClient(ocs, agent_addr);
+    $('#op_status').html('Updating...');
+    client.status(agent_op).then(function (args, kwargs) {
+        ocs_log('status returned.');
+        $('#op_status').html(args[2]['status']);
+        debugs['status'] = args;});
 }

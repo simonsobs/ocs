24e474ad5b1a5c655b9dcaac957aabcd97d43bd4
Brian Koopman <brian.koopman@yale.edu>
2020-02-25 15:51:18 -0500

    Improve test coverage for rename

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -4,10 +4,56 @@ from pathlib import Path
 
 from ocs import rename
 
-def test_rename_files(tmpdir):
+def test_rename_main(tmpdir):
+    """Test rename.main(), should hit most of the rename module. Tests renaming
+    a directory with a single file in it.
+
+    """
     p = tmpdir.mkdir("data")
     Path(os.path.join(p, "2020-01-01-12-00-00.g3")).touch()
 
-    rename.rename_files(p)
+    rename.main(p, verbose=1)
 
     assert os.path.isfile(os.path.join(p, "1577880000.g3"))
+
+def test_rename_single_file(tmpdir):
+    """target can be a single file, so we test tarting a file instead of a
+    directory.
+
+    """
+    p = tmpdir.mkdir("data")
+    filename = "2020-01-01-12-00-00.g3"
+    Path(os.path.join(p, filename)).touch()
+
+    rename.main(os.path.join(p, filename))
+
+    assert os.path.isfile(os.path.join(p, "1577880000.g3"))
+
+def test_nonmatching_filename(tmpdir):
+    """Files must match the datestring time format, else we won't run them.
+    Test that we skip these files.
+
+    """
+    p = tmpdir.mkdir("data")
+    filename = "2020-01-01-12-00-00_non_match.g3"
+    Path(os.path.join(p, filename)).touch()
+
+    rename.main(os.path.join(p, filename))
+    rename.main(os.path.join(p, filename), verbose=1)
+
+    # file should remain unaffected
+    assert os.path.isfile(os.path.join(p, filename))
+
+def test_file_collision(tmpdir):
+    """If we somehow end up with a matching timestring we shouldn't overwrite
+    the existing file. This checks an error is raised in this case.
+
+    """
+    p = tmpdir.mkdir("data")
+    filename = "2020-01-01-12-00-00.g3"
+    Path(os.path.join(p, filename)).touch()
+    # Make our own renamed file to collide
+    Path(os.path.join(p, "1577880000.g3")).touch()
+
+    with pytest.raises(OSError):
+        rename.main(os.path.join(p, filename))

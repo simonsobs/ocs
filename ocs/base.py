c2a1636351702e606421c9b8b113e1b709f8f24c
jlashner <jlashner@gmail.com>
2020-11-11 22:26:47 -0800

    Implements PR suggestions

--- ocs/base.py
+++ ocs/base.py
@@ -1,4 +1,5 @@
 import ocs
+from enum import Enum
 
 RET_VALS = {
     'OK': 0,
@@ -9,3 +10,15 @@ RET_VALS = {
 OK = RET_VALS['OK']
 ERROR = RET_VALS['ERROR']
 TIMEOUT = RET_VALS['TIMEOUT']
+
+class OpCode(Enum):
+    """
+    Enumeration of OpSession states.
+    """
+    NONE = 1
+    STARTING = 2
+    RUNNING = 3
+    STOPPING = 4
+    SUCCEEDED = 5
+    FAILED = 6
+    EXPIRED = 7

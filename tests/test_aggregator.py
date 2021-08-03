d9e1aaac43acfbfeddd02333de4fb24765a659a8
Brian Koopman <brian.koopman@yale.edu>
2021-08-03 14:52:20 -0400

    Add tests for aggregator.make_filename

--- tests/test_aggregator.py
+++ tests/test_aggregator.py
@@ -1,10 +1,13 @@
+import os
 import time
 import pytest
+
+from unittest.mock import patch
 
 import so3g
 from spt3g import core
 
-from ocs.agent.aggregator import Provider, g3_cast
+from ocs.agent.aggregator import Provider, g3_cast, make_filename
 
 def test_passing_float_in_provider_to_frame():
     """Float is the expected type we should be passing.
@@ -363,3 +366,38 @@ def test_g3_cast():
     for x in incorrect_tests:
         with pytest.raises(TypeError) as e_info:
             g3_cast(x)
+
+
+def test_make_filename_directory_creation(tmpdir):
+    """make_filename() should be able to create directories to store the .g3
+    files in.
+
+    """
+    test_dir = os.path.join(tmpdir, 'data')
+    fname = make_filename(test_dir)
+    # Test we could make the subdir
+    os.path.isdir(os.path.basename(fname))
+
+
+def test_make_filename_directory_creation_no_subdirs(tmpdir):
+    """make_filename() should raise a FileNotFoundError if make_subdirs is
+    False.
+
+    """
+    test_dir = os.path.join(tmpdir, 'data')
+    with pytest.raises(FileNotFoundError):
+        make_filename(test_dir, make_subdirs=False)
+
+
+@patch('os.makedirs', side_effect=PermissionError('mocked permission error'))
+def test_make_filename_directory_creation_permissions(tmpdir):
+    """make_filename() should raise a PermissionError if it runs into one when
+    making the directories.
+
+    Here we mock raising the PermissionError on the makedirs call.
+
+    """
+    test_dir = os.path.join(tmpdir, 'data')
+    with pytest.raises(PermissionError) as e_info:
+        make_filename(test_dir)
+    assert str(e_info.value) == 'mocked permission error'

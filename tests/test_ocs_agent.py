d59e24098fdcdc1e569178ae0e40ca49e80e71af
Brian Koopman <brian.koopman@yale.edu>
2021-09-03 11:45:40 -0400

    Use pytest-twisted to test OCSAgent.wait()

--- tests/test_ocs_agent.py
+++ tests/test_ocs_agent.py
@@ -2,18 +2,24 @@ from ocs.ocs_agent import OCSAgent
 
 from unittest.mock import MagicMock
 
+import pytest
+import pytest_twisted
+
 from pathlib import Path
 
-def tfunc(a):
+def tfunc(session, a):
     print(a)
     Path('./test.txt').touch()
+    return True, 'words'
     
-
+@pytest_twisted.inlineCallbacks
 def test_agent():
+    # Could be a test fixture to setup agent
     mock_config = MagicMock()
     mock_site_args = MagicMock()
     mock_site_args.log_dir = "./"
     a = OCSAgent(mock_config, mock_site_args, address='test.address')
+
     # insert a task into the Agent
     # TODO make 'test' an AgentTask (or maybe just call a.register_task()?
     #a.tasks = {'test_task': 'test'}
@@ -26,3 +32,8 @@ def test_agent():
     # actually wait for a respons here in this test...
     # TODO: do we want to do that? how would we do that?
     print(a.sessions['test_task'].d)
+    #res = yield a.sessions['test_task'].d
+    #print('result:', res)
+    print(a.wait('test_task'))
+    res = yield a.wait('test_task')
+    print('result:', res)

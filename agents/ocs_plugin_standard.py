55daead508662313f9ee8131278ce737d38fcff3
Brian Koopman <brian.koopman@yale.edu>
2021-10-14 11:22:36 -0400

    Add InfluxDBAgent to ocs plugin list

--- agents/ocs_plugin_standard.py
+++ agents/ocs_plugin_standard.py
@@ -13,5 +13,6 @@ for n,f in [
         ('AggregatorAgent', 'aggregator/aggregator_agent.py'),
         ('HostMaster', 'host_master/host_master.py'),
         ('FakeDataAgent', 'fake_data/fake_data_agent.py'),
+        ('InfluxDBAgent', 'influxdb_publisher/influxdb_publisher.py'),
 ]:
     ocs.site_config.register_agent_class(n, os.path.join(root, f))

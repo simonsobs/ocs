4a3d96a73879c43bdcb87d95e3c3b75e52033520
Brian Koopman <brian.koopman@yale.edu>
2021-10-18 12:49:46 -0400

    Setup integration testing example with fake-data agent

---
+++ tests/integration/util2.py
@@ -0,0 +1,51 @@
+import os
+import time
+import pytest
+import signal
+import subprocess
+import coverage.data
+
+# Set the OCS_CONFIG_DIR so we read the local default.yaml file always
+os.environ['OCS_CONFIG_DIR'] = os.getcwd()
+
+
+def create_agent_runner_fixture(agent_path, agent_name):
+    """Create a pytest fixture for running a given OCS Agent.
+
+    Parameters:
+        agent_path (str): Relative path to Agent,
+            i.e. '../agents/fake_data/fake_data_agent.py'
+        agent_name (str): Short, unique name for the agent
+
+    """
+    @pytest.fixture()
+    def run_agent(cov):
+        env = os.environ.copy()
+        env['COVERAGE_FILE'] = f'.coverage.agent.{agent_name}'
+        env['OCS_CONFIG_DIR'] = os.getcwd()
+        agentproc = subprocess.Popen(['coverage', 'run',
+                                      '--rcfile=./.coveragerc',
+                                      agent_path,
+                                      '--site-file',
+                                      './default.yaml'],
+                                     env=env,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE,
+                                     preexec_fn=os.setsid)
+
+        # wait for Agent to connect
+        time.sleep(1)
+
+        yield
+
+        # shutdown Agent
+        agentproc.send_signal(signal.SIGINT)
+        time.sleep(1)
+
+        # report coverage
+        agentcov = coverage.data.CoverageData(
+            basename=f'.coverage.agent.{agent_name}')
+        agentcov.read()
+        cov.get_data().update(agentcov)
+
+    return run_agent

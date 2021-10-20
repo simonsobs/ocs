d6836f35406c087bb57f20205b698985ddecfb0a
Brian Koopman <brian.koopman@yale.edu>
2021-10-20 13:40:46 -0400

    Remove startup_sleep from agent fixture
    The Client fixture now attempts reconnects up to a certain timeout, which is a
    faster way of waiting for the Agent to come up, since we can just check
    rapidly, rather than way some time that fluctuates given the load of the
    system for the Agent to finish starting up.

--- tests/integration/test_aggregator_agent_integration.py
+++ tests/integration/test_aggregator_agent_integration.py
@@ -14,8 +14,7 @@ pytest_plugins = ("docker_compose")
 wait_for_crossbar = create_crossbar_fixture()
 run_agent = create_agent_runner_fixture(
     '../agents/aggregator/aggregator_agent.py',
-    'aggregator-local',
-    startup_sleep=2)
+    'aggregator-local')
 client = create_client_fixture('aggregator-local')
 
 

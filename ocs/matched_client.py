6d32b05785d76018c82b50d6e1cc685dc8466f6d
Brian Koopman <brian.koopman@yale.edu>
2021-11-01 17:11:27 -0400

    Mark MatchedClient as deprecated

--- ocs/matched_client.py
+++ ocs/matched_client.py
@@ -1,6 +1,12 @@
+import deprecation
+
 from ocs.ocs_client import OCSClient
 
 
+@deprecation.deprecated(
+    deprecated_in='v0.9.0',
+    details="Renamed to OCSClient"
+)
 class MatchedClient(OCSClient):
     def __init__(self, instance_id, **kwargs):
         super().__init__(instance_id, **kwargs)

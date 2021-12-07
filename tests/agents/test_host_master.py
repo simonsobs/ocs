998159c3400750cc430e8c417e424daf6a58243b
Matthew Hasselfield <mhasself@users.noreply.github.com>
2021-12-07 11:36:46 -0500

    tests: remove broken references in favor of HostManager

--- tests/agents/test_host_master.py
+++ tests/agents/test_host_master.py
@@ -1,15 +1,15 @@
 import sys
-sys.path.insert(0, '../agents/host_master/')
-from host_master import HostMaster
+sys.path.insert(0, '../agents/host_manager/')
+from host_manager import HostManager
 
 from agents.util import create_session, create_agent_fixture
 
 
 # fixtures
-agent = create_agent_fixture(HostMaster)
+agent = create_agent_fixture(HostManager)
 
 
-def test_host_master_update_not_running(agent):
+def test_host_manager_update_not_running(agent):
     session = create_session('update')
     res = agent.update(session, params=None)
     assert res[0] is False
--- tests/integration/test_host_master_agent_integration.py
+++ tests/integration/test_host_master_agent_integration.py
@@ -13,17 +13,17 @@ pytest_plugins = ("docker_compose")
 
 
 wait_for_crossbar = create_crossbar_fixture()
-run_agent = create_agent_runner_fixture('../agents/host_master/host_master.py',
-                                        'master-host-1',
+run_agent = create_agent_runner_fixture('../agents/host_manager/host_manager.py',
+                                        'host-manager-1',
                                         args=['--log-dir',
                                               os.path.join(os.getcwd(),
                                                            'log/')])
-client = create_client_fixture('master-host-1')
+client = create_client_fixture('host-manager-1', timeout=5)
 
 
 @pytest.mark.integtest
-def test_host_master_agent_master(wait_for_crossbar, run_agent, client):
+def test_host_manager_agent_manager(wait_for_crossbar, run_agent, client):
     # Startup is always true, so let's check it's running
-    resp = client.master.status()
+    resp = client.manager.status()
     print(resp)
     assert resp.session['op_code'] == OpCode.RUNNING.value

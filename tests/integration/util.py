1c993ad493ba5213a3379b959916f9494962fd5d
Matthew Hasselfield <mhasself@users.noreply.github.com>
2021-12-07 11:34:53 -0500

    tests: create_agent_runner_fixture is more careful and verbose
    When launching an Agent, check that the process started up; and that
    it shuts down.  Print more useful error messages.

--- tests/integration/util.py
+++ tests/integration/util.py
@@ -12,6 +12,9 @@ from urllib.error import URLError
 from ocs.ocs_client import OCSClient
 
 
+SIGINT_TIMEOUT = 5
+
+
 def create_agent_runner_fixture(agent_path, agent_name, args=None):
     """Create a pytest fixture for running a given OCS Agent.
 
@@ -40,14 +43,28 @@ def create_agent_runner_fixture(agent_path, agent_name, args=None):
                                      stderr=subprocess.PIPE,
                                      preexec_fn=os.setsid)
 
-        # wait for Agent to startup 
+        def raise_subprocess(msg):
+            stdout, stderr = agentproc.stdout.read(), agentproc.stderr.read()
+            print(f'Here is stdout from {agent_name}:\n{stdout}')
+            print(f'Here is stderr from {agent_name}:\n{stderr}')
+            raise RuntimeError(msg)
+
+
+        # Wait briefly then make sure subprocess hasn't already exited.
         time.sleep(1)
+        if agentproc.poll() is not None:
+            raise_subprocess(f"Agent failed to startup, cmd: {cmd}")
 
         yield
 
         # shutdown Agent
         agentproc.send_signal(signal.SIGINT)
-        time.sleep(1)
+
+        try:
+            agentproc.communicate(timeout=SIGINT_TIMEOUT)
+        except subprocess.TimeoutExpired:
+            raise_subprocess(f'Agent did not terminate within {SIGINT_TIMEOUT} '
+                             ' seconds on SIGINT.')
 
         # report coverage
         agentcov = coverage.data.CoverageData(
@@ -125,7 +142,7 @@ def create_client_fixture(instance_id, timeout=30):
         while attempts < timeout:
             try:
                 client = OCSClient(instance_id)
-                break
+                return client
             except RuntimeError as e:
                 print(f"Caught error: {e}")
                 print("Attempting to reconnect.")
@@ -133,6 +150,6 @@ def create_client_fixture(instance_id, timeout=30):
             time.sleep(1)
             attempts += 1
 
-        return client
+        raise RuntimeError(f"Failed to connect to {instance_id} after {timeout} attempts.")
 
     return client_fixture

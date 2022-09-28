b07cff586a2607c81ce6212794a43e7482eb9754
Brian Koopman <brian.koopman@yale.edu>
2022-09-28 11:34:52 -0400

    Allow running agents outside of plugins
    In the process we replace manually handling args with argparse.

--- ocs/agent_cli.py
+++ ocs/agent_cli.py
@@ -13,17 +13,23 @@ from ocs import site_config
 
 DESCRIPTION = """
 This script provides a quick way to start an OCS Agent. You will need to set
-OCS_CONFIG_DIR environment variable to the directory containing default.yaml,
-or else use ``--site-*`` options to specify your configuration.
+``OCS_CONFIG_DIR`` environment variable to the directory containing
+default.yaml, or else use ``--site-*`` options to specify your configuration.
 
 To start an Agent, run::
 
-  %(prog)s --instance-id INSTANCE_ID
+  ocs-agent-cli --instance-id INSTANCE_ID
 
 ``ocs-agent-cli`` will also inspect environment variables for commonly passed
 arguments to facilitate configuration within Docker containers. Supported
 arguments include, ``--instance-id`` as ``INSTANCE_ID``, ``--site-hub`` as
 ``SITE_HUB``, and ``--site-http`` as ``SITE_HTTP``.
+
+``ocs-agent-cli`` relies on the Agent being run belonging to an OCS Plugin. If
+the Agent is not an OCS Plugin it can be run directly using both the
+``--agent`` and ``--entrypoint`` flags. For example::
+
+  ocs-agent-cli --agent my_agent.py --entrypoint main --instance-id my-agent-1
 
 """
 
@@ -32,6 +38,15 @@ def _get_parser():
     parser = argparse.ArgumentParser(
         description=DESCRIPTION,
         formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # Passed through to Agent
+    parser.add_argument('--instance-id', default=None, help="Agent unique instance-id. E.g. 'aggregator' or 'fakedata-1'.")
+    parser.add_argument('--site-hub', default=None, help="Site hub address.")
+    parser.add_argument('--site-http', default=None, help="Site HTTP address.")
+
+    # Not passed through to Agent
+    parser.add_argument('--agent', default=None, help="Path to non-plugin OCS Agent.")
+    parser.add_argument('--entrypoint', default=None, help="Agent module entrypoint function.")
 
     return parser
 
@@ -62,7 +77,7 @@ def build_agent_list():
 
     """
     discovered_plugins = entry_points(group='ocs.plugins')
-    print(discovered_plugins)
+    print("Installed OCS Plugins:", [x.name for x in discovered_plugins])
     agents = {}
     for name in discovered_plugins.names:
         (plugin, ) = discovered_plugins.select(name=name)
@@ -88,55 +103,87 @@ def build_agent_list():
 
 
 def main(args=None):
-    # Grab instance-id from ENV for running in Docker
-    id_env = os.environ.get("INSTANCE_ID", None)
-
     # Grab commandline arguments
     if args is None:
-        argv = sys.argv[1:]
+        args = sys.argv[1:]
 
-    # Split and flatten argv list
-    _split = [x.split('=') for x in argv]
-    argv = [x for y in _split for x in y]
+    parser = _get_parser()
+    # pre_args relevant to ocs-agent-cli, post_args passed to Agent entrypoint
+    pre_args, post_args = parser.parse_known_args(args)
 
-    # Inject ENV based instance-id only if not passed on cli
-    if "--instance-id" not in argv:
-        argv.extend(["--instance-id", id_env])
+    # Required arguments
+    if pre_args.instance_id is not None:
+        # Re-add instance-id to post_args
+        post_args.extend(["--instance-id", pre_args.instance_id])
+    else:
+        # Grab instance-id from ENV for running in Docker
+        id_env = os.environ.get("INSTANCE_ID", None)
+
+        # Inject ENV based instance-id only if not passed on cli
+        post_args.extend(["--instance-id", id_env])
+
+        # instance-id is always required
         if id_env is None:
             print("--instance-id (or $INSTANCE_ID) not provided. Exiting.")
             sys.exit(1)
 
-    # Inject optional argv from ENV
-    optional_env = {"--site-hub": "SITE_HUB" ,
-                    "--site-http": "SITE_HTTP"}
+    # Optional arguments
+    # Add additional optional arguments by appending to optional_env
+    # Format is {"arg name within argparse": "ENVIRONMENT VARIABLE NAME"}
+    # E.g. --my-new-arg should be {"my_new_arg": "MY_NEW_ARG"}
+    optional_env = {"site_hub": "SITE_HUB" ,
+                    "site_http": "SITE_HTTP"}
 
-    for _arg, _var in optional_env.items():
+    for _name, _var in optional_env.items():
+        # Args passed on cli take priority
+        _arg = vars(pre_args)[_name]
+        _flag = f"--{_name}".replace('_', '-')
+        if _arg is not None:
+            post_args.extend([_flag, _arg])
+            continue
+
+        # Get from ENV if not passed w/flag
         set_var = os.environ.get(_var, None)
-        if set_var is not None and _arg not in argv:
-            argv.extend([_arg, set_var])
+        if set_var is not None:
+            post_args.extend([_flag, set_var])
 
-    print('ARGS', argv)
-    parser = _get_parser()
+    print('Args:', post_args)  # mostly to debug, but also useful
+    # Handle running agent directly (outside of plugin package)
+    if pre_args.agent:
+        agent_file = pre_args.agent
 
-    # Note this is only used to lookup the agent-class, argv is passed to the
-    # agent's entrypoint below when calling start(), which includes injected
-    # ENV based arguments.
-    args = site_config.parse_args(agent_class='*host*',
-                                  parser=parser, args=argv)
+        # Entrypoint required if running outside of plugin system
+        if pre_args.entrypoint is None:
+            print("--entrypoint not provided. Exiting")
+            sys.exit(1)
 
-    # Determine agent-class from instance-id
-    (site_, host_, instance) = site_config.get_config(args)
-    agent_class = instance.data['agent-class']
+        entrypoint = pre_args.entrypoint
 
-    # Import agent's entrypoint and execute
-    agents = build_agent_list()
-    agent_info = agents[agent_class]
-    _module = agent_info["module"]
-    _entry = agent_info.get("entry_point", "main")
+        # Insert agent into path for import
+        script_path = os.path.dirname(agent_file)
+        sys.path.insert(0, script_path)
+        mod = importlib.import_module(os.path.basename(agent_file)[:-3])
+    else:
+        # Note this is only used to lookup the agent-class, post_args are
+        # passed to the agent's entrypoint below when calling start(), which
+        # includes injected ENV based arguments.
+        args = site_config.parse_args(agent_class='*host*',
+                                      parser=None, args=post_args)
 
-    mod = importlib.import_module(_module)
-    start = getattr(mod, _entry)  # This is the start function.
-    start(argv)  # noqa: F821
+        # Determine agent-class from instance-id
+        (site_, host_, instance) = site_config.get_config(args)
+        agent_class = instance.data['agent-class']
+
+        # Import agent's entrypoint and execute
+        agents = build_agent_list()
+        agent_info = agents[agent_class]
+        _module = agent_info["module"]
+        entrypoint = agent_info.get("entry_point", "main")
+
+        mod = importlib.import_module(_module)
+
+    start = getattr(mod, entrypoint)  # This is the start function.
+    start(post_args)  # noqa: F821
 
 
 if __name__ == '__main__':

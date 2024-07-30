10ec779a469476123f4794d4a6e1e3187c04d06b
Brian Koopman <brian.koopman@yale.edu>
2024-07-30 13:04:08 -0500

    Update versioneer to version 0.29

--- ocs/__init__.py
+++ ocs/__init__.py
@@ -2,6 +2,5 @@ from .base import OK, ERROR, TIMEOUT, ResponseCode, OpCode  # noqa: F401
 
 from . import site_config  # noqa: F401
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']

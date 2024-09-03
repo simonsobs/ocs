9d7caf0d885314f48395fba1cfc48cb3f9d732b0
Brian Koopman <brian.koopman@yale.edu>
2024-09-03 15:03:24 -0400

    Adjust setuptools-scm version scheme

--- ocs/__init__.py
+++ ocs/__init__.py
@@ -12,7 +12,7 @@ try:
 
     # This will fail with LookupError if the package is not installed in
     # editable mode or if Git is not installed.
-    __version__ = get_version(root="..", relative_to=__file__)
+    __version__ = get_version(root="..", relative_to=__file__, version_scheme="no-guess-dev")
 except (ImportError, LookupError):
     # As a fallback, use the version that is hard-coded in the file.
     try:

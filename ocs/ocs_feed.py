cbede431b75f8a3a23a97dbd1cfccea0f7f2e6bc
Brian Koopman <brian.koopman@yale.edu>
2026-04-06 14:06:30 -0400

    Add checks within Feed on influxdb_tags

--- ocs/ocs_feed.py
+++ ocs/ocs_feed.py
@@ -217,6 +217,9 @@ class Feed:
                 Feed.verify_data_field_string(k)
                 Feed.verify_message_data_type(v)
 
+            # check influxdb_tags
+            Feed.verify_influxdb_tags(message)
+
             # Data is stored in Block objects
             block_name = message['block_name']
             try:
@@ -247,6 +250,40 @@ class Feed:
             except TransportLost:
                 self.agent.log.error('Could not publish to Feed. TransportLost. '
                                      + 'crossbar server likely unreachable.')
+
+    @staticmethod
+    def verify_influxdb_tags(message):
+        """Check the 'influxdb_tags' to make sure all the needed information is
+        provided.
+
+        This checks to make sure each tag has a '_field' provided, and that
+        each field has a corresponding tag.
+
+        Args:
+            message (dict):
+                'message' dictionary value published (see Feed.publish_message for details).
+
+        Raises:
+            ValueError: If the 'influxdb_tags' provided in the message do not
+                meet the required format.
+
+        """
+        tags = message.get('influxdb_tags')
+        if tags is None:
+            return
+
+        # check '_field' supplied with each tag
+        for v in tags.values():
+            if '_field' not in v:
+                error_msg = f"'_field' not supplied with 'influxdb_tags' for tag set {v}"
+                raise ValueError(error_msg)
+
+        # check that all fields have a corresponding tag
+        tag_fields = tags.keys()
+        for k in message['data'].keys():
+            if k not in tag_fields:
+                error_msg = f"'influxdb_tags' does not contain tags for '{k}'"
+                raise ValueError(error_msg)
 
     @staticmethod
     def verify_message_data_type(value):
